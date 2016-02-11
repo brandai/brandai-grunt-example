25f77f79592f8f9c060f5ce2fd45d1b2989a8ea3
Amit Halberstam <amithalb@gmail.com>
2016-02-11 11:12:31 +0200

    - use grunt-curl - use typography style mixin

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,16 @@ module.exports = function(grunt) {
         }
       }
     },
-    downloadfile: {
-      files: [
-        {
-          url: 'https://assets.brand.ai/acme-demo/_style-params.scss',
-          dest: 'scss'
-        }
-      ]
+    'curl-dir': {
+      'styles': {
+        src: ['https://assets.brand.ai/acme-demo/_style-params.scss'],
+        dest: 'scss' // the sass source folder
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-downloadfile');
+  grunt.loadNpmTasks('grunt-curl');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('styles', ['downloadfile']);
+  grunt.registerTask('styles', ['curl-dir:styles']);
 };
